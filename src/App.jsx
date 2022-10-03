fc7dd5338cfc5a92ef03b4dd998f002514331bfe
thanhluan0307 <93823036+thanhluan0307@users.noreply.github.com>
2022-10-03 21:35:50 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,11 @@ function App() {
   const [button,setButton] = useState(true)
   const [done,setDone] = useState(0)
   const [deadLine,setDeadLine] = useState(0)
+  const [searchJob,setSearchJob] = useState('')
+  const [Filter,setFilter] = useState('sum')
   let i
   const currentime = new Date()
-
-
-  
-
-  
+  console.log(Filter)
   const handleAdd = () =>{
     if(!value || !time) return alert('chua co job')
     setJobs(pre => {
@@ -60,6 +58,7 @@ function App() {
   return (
 
     <div className='wrapper'>
+      <Header title="Deadline là ngày hiện tại"/>
       <Header title="Công việc hoàn thành" data={done}/>
       <Header title="Công việc deadline" data={deadLine}/>
       Jobs:<input 
@@ -73,12 +72,21 @@ function App() {
             onInput={e => setTime(e.target.value)}
             /> <br />
       Search:<input 
-              type="text"  
+              type="text" 
+              onChange={e => setSearchJob(e.target.value)} 
             /> <br /> <br />
       {button ? 
         <Button value="Add" onClick={handleAdd}/> :
         <Button value="Update" onClick={() => handleUpdate(i)}/>
       } <br />
+      <select 
+          onChange={(e)=> {setFilter(e.target.value)}}
+          value={Filter} 
+          id="12">
+          <option value="done">Done</option>
+          <option value="deadline">DeadLine</option>
+          <option value="sum">Sum</option>
+      </select>
    
       <div>
         <table>
@@ -86,18 +94,41 @@ function App() {
             <tr>
               <th>Jobs</th>
               <th>Time</th>
-              <th>    </th>
+              <th>^.^</th>
             </tr>
           </thead>
           <tbody>
-            {jobs.map(function(job,index){
-                return (
-                  <tr key={index} className={currentime > new Date(job.time) ? "done" : "deadline"}>
-                    <th onClick={() => displayBtn(index)}>{job.job}</th>
-                    <th>{job.time}</th>
+            {jobs.filter((val) => {
+
+              if(searchJob === "") {
+                return val
+              }else if  (val.job.toLowerCase().includes(searchJob.toLowerCase())) {
+                return val
+              }
+            }).map(function(val,index){
+                if(Filter === "done" && currentime > new Date(val.time)) {
+                    return (
+                      <tr key={index} className="done">
+                        <th onClick={() => displayBtn(index)}>{val.job}</th>
+                        <th>{val.time}</th>
+                        <th><Button value="Delete" onClick={() => {handleDelete(index)}}/></th>
+                      </tr>
+                    )
+                }else if(Filter === "deadline" && currentime < new Date(val.time)) {
+                  return (
+                    <tr key={index} className="deadline">
+                      <th onClick={() => displayBtn(index)}>{val.job}</th>
+                      <th>{val.time}</th>
+                      <th><Button value="Delete" onClick={() => {handleDelete(index)}}/></th>
+                    </tr>
+                  )
+                }else {
+                  <tr key={index}>
+                    <th onClick={() => displayBtn(index)}>{val.job}</th>
+                    <th>{val.time}</th>
                     <th><Button value="Delete" onClick={() => {handleDelete(index)}}/></th>
-                </tr>
-                )
+                  </tr>
+                }    
               })
             }
           </tbody>
